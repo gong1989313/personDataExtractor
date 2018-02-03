9e9f2ac1a34fc9d2c2a00a3f4ba109bd71da63f7
Administrator <Administrator@F1RU37WELLAEON0>
2018-02-03 23:21:52 +0800

    fix

--- src/main/java/com/gxq/learn/extractor/util/FileUtil.java
+++ src/main/java/com/gxq/learn/extractor/util/FileUtil.java
@@ -1,7 +1,6 @@
 package com.gxq.learn.extractor.util;
 
 import java.io.IOException;
-import java.nio.file.CopyOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
